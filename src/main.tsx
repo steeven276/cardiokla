7b10ca48fe77c3ec6ddc3f344f156ef1104706df
Raphaël <raphael.dhote@gmail.com>
2023-02-24 14:17:20 +0100

    feat(main): delete App and implement router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./common.css";
+import { Home } from "./components/Home/Home";
+import { Patient } from "./components/Patient/Patient";
+import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import frFR from "antd/es/locale/fr_FR";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import initPatientsStorage from "./data/initPatientsStorage";
+import initMedicalDataStorage from "./data/initMedicalDataStorage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/patient/:id", element: <Patient /> },
+]);
+
+initMedicalDataStorage();
+initPatientsStorage();
+
+const rootElement = document.getElementById("root");
+
+if (rootElement !== null) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <ConfigProvider locale={frFR}>
+        <RouterProvider router={router} />
+      </ConfigProvider>
+    </React.StrictMode>
+  );
+}
