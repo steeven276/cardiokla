f96197dc8164bd7f031ec454da293b8ed693b348
Raphaël <raphael.dhote@gmail.com>
2023-03-17 11:06:44 +0100

    feat(Patient): fill Patient page to show cardiac frequency graph

--- src/components/Patient/Patient.tsx
+++ src/components/Patient/Patient.tsx
@@ -1,9 +1,65 @@
-import { FunctionComponent } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { CartesianGrid, Label, Line, LineChart, XAxis, YAxis } from "recharts";
 import "./Patient.css";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { usePatients } from "../../hooks/usePatients";
+import { usePatientMedicalData } from "../../hooks/usePatientMedicalData";
+import { CardiacFrequencyData } from "../../types/MedicalData";
 
-export const Patient: FunctionComponent = () => {
-  const { id } = useParams<{ id: string }>();
+export const Patient = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  return <h1>Patient with id {id}</h1>;
+  const patients = usePatients();
+
+  const patient = patients.find((patient) => patient.id === id);
+
+  if (patient === undefined) {
+    return <h1>Patient not found</h1>;
+  }
+
+  const medicalData = usePatientMedicalData(patient.id);
+
+  const getDisplayedDate = (dataPoint: CardiacFrequencyData) => {
+    const date = new Date(dataPoint.datetime);
+
+    return new Intl.DateTimeFormat().format(date);
+  };
+
+  return (
+    <>
+      <>
+        <div className="header">
+          <div onClick={() => navigate(-1)}>
+            <ArrowLeftOutlined className="back-icon" />
+          </div>
+          <div className="info-container">
+            <h1>{patient.name}</h1>
+            <div className="email-birthdate-container">
+              <h2>E-mail: {patient.email}</h2>
+              <div className="spacer"></div>
+              <h2>Birthdate: {patient.birthdate}</h2>
+            </div>
+          </div>
+        </div>
+        {medicalData !== null && (
+          <div className="chart-container">
+            <LineChart width={1000} height={500} data={medicalData}>
+              <Line type="monotone" dataKey="value" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" />
+              <XAxis dataKey={getDisplayedDate}>
+                <Label value="Date" position="bottom" offset={0} />
+              </XAxis>
+              <YAxis>
+                <Label value={`Cardiac frequency (bpm)`} position="left" />
+              </YAxis>
+            </LineChart>
+          </div>
+        )}
+      </>
+    </>
+  );
 };
+
+export default Patient;
